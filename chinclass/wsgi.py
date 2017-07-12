166ff8d9c590043575b029b4fcc69414b613d70d
Ondra Lengal <ondra.lengal@gmail.com>
2017-07-12 19:42:02 +0800

    change name chinese -> chinclass

--- chinclass/wsgi.py
+++ chinclass/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chinese project.
+WSGI config for chinclass project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chinese.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chinclass.settings")
 
 application = get_wsgi_application()
